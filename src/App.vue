5dac6fa020d60b757018d56357fa20df262e7dd1
jdrly <jd@jandrly.cz>
2025-12-18 20:58:56 +0100

    Refactor App.vue to integrate Convex query for task management, update package.json scripts to use concurrently for development, and enhance Prettier configuration.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
+import { useConvexQuery } from 'convex-vue';
+import { api } from '../convex/_generated/api';
+
+const { data, isPending } = useConvexQuery(api.tasks.get);
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <Button>Click me</Button>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <h1>You did it!</h1>
+    <Button>Click me</Button>
+    <p>Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the documentation</p>
+    <span v-if="isPending"> Loading... </span>
+    <ul v-else>
+        <li v-for="todo in data" :key="todo._id">{{ todo.text }} {{ todo.isCompleted ? '☑' : '☐' }}</li>
+    </ul>
 </template>
 
 <style scoped></style>
