145f2e534cbb92119ce7134521103e8b9d83a42e
jdrly <jd@jandrly.cz>
2025-12-18 20:17:22 +0100

    Add Tailwind CSS and UI components; update TypeScript config and Vite setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { convexVue } from 'convex-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/app.css'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(convexVue, {
+    url: 'your-convex-deployment-url'
+  })
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
+    tsconfigPaths(),
   ],
   resolve: {
     alias: {
